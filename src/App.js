da1437ad3e33e37ca5dea3b38bc3e0917c00c676
Abdulrahman Emad Eldein Alshafei <shafei@pop-os.localdomain>
2021-01-26 00:16:52 +0200

    react router / custom layout / footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import "./default.scss";
-import Header from "./components/Header";
+
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+
 import Homepage from "./pages/Homepage";
+import Registeration from "./pages/Registeration";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+
+        <Route
+          path="/registeration"
+          render={() => (
+            <MainLayout>
+              <Registeration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
